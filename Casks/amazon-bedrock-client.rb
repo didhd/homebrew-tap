2b106519d1639ab270d62c3d29aecfe3ca3af68c
Anka <runner@iad20-fj920-c25d253f-a3c7-4b39-a83a-5f1b2dda52d0-F2DB59E7DA06.local>
2026-09-18 00:05:55 +0000

    amazon-bedrock-client: update URL

--- Casks/amazon-bedrock-client.rb
+++ Casks/amazon-bedrock-client.rb
@@ -1,8 +1,8 @@
 cask "amazon-bedrock-client" do
-  version "2.0.0"
-  sha256 "cb27558a7326eda26b2dd642ebaef38b4bbf7fa590d3454893c08c9b0914f66e"
+  version "2.0.1"
+  sha256 "e9618edb835990e96731a9d5ee938b63befb56d942e7d2719f468c90e6a07e05"
 
-  url "https://github.com/aws-samples/amazon-bedrock-client-for-mac/releases/download/v2.0.0/Amazon.Bedrock.Client.for.Mac.dmg"
+  url "https://github.com/aws-samples/amazon-bedrock-client-for-mac/releases/download/v2.0.1/Amazon.Bedrock.Client.for.Mac.dmg"
   name "Amazon Bedrock Client"
   desc "Client application for Amazon Bedrock generative AI models"
   homepage "https://github.com/aws-samples/amazon-bedrock-client-for-mac"
